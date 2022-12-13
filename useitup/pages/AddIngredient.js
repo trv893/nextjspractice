ee11766e2203ce7c56952f22d7b130c36cb83412
trv893 <trevorhilimire@gmail.com>
2022-12-13 15:10:19 -0500

    sets up state for ingredient submit

--- useitup/pages/AddIngredient.js
+++ useitup/pages/AddIngredient.js
@@ -1,17 +1,31 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import { useState } from 'react';
 
 function AddIngredientNoLogin() {
+  const [ingredient, setIngredient] = useState('');
+
+  const handleIngredientChange = (e) => {
+    setIngredient(e.target.value);
+
+  }
+
+  const handleIngredientSubmit = (e) => {
+    console.log(ingredient)
+  }
+
   return (
     <>
       <InputGroup className="mb-3 w-25">
         <Form.Control
+          onChange={handleIngredientChange}
+          type="text"
           placeholder="Type Ingredient Here..."
           aria-label="Ingredient to search"
           aria-describedby="ingredient-search"
         />
-        <Button variant="outline-secondary" id="AddIngredientNoLoginButton"
+        <Button onClick={handleIngredientSubmit} variant="outline-secondary" id="AddIngredientNoLoginButton"
         type='submit'>
           Find Recipies
         </Button>
